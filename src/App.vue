9f7af163c808b3c79fda8e7d995acbe2649bfacb
Vitali Zippedlt <vitaliy.zippeld@ewave.com>
2017-05-08 16:50:50 +0300

    master: initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <editor></editor>
+    </div>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor'
+
+export default {
+  name: 'app',
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style lang="scss">
+$wrapper__background-color: #e1e1e1;
+#app {
+  & {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: $wrapper__background-color;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+  }
+  & > .wrapper {
+    max-width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+    background-color: #fff;
+  }
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="editor-companent">
+            <button>Добавить новую</button>
+            <input type="file" @change="onFileChange">
+            <input type="text" v-model="postTitle">
+            <textarea v-model="postText"></textarea>
+            <img :src="image"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'editor',
+    data () {
+        return {
+            buttonText: 'Добавить новую',
+            image: null,
+            postTitle: '',
+            postText: ''
+        }
+    },
+    methods: {
+        onFileChange (e) {
+            let files = e.target.files || e.dataTransfer.files;
+            if (files.length) {
+                this.createImage(files[0])
+            }
+        },
+        createImage (file) {
+            let reader = new FileReader();
+
+            reader.onload = (e) => {
+                this.image = e.target.result
+            };
+
+            reader.readAsDataURL(file)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
